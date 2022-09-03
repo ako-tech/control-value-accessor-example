7bd95898c6ff5a6272d1e1dfff3ac470be7d5648
akotech <akotech20@gmail.com>
2022-09-03 18:35:49 +0200

    impl ControlValueAccessor

--- src/app/color-selector/color-selector.component.ts
+++ src/app/color-selector/color-selector.component.ts
@@ -1,19 +1,45 @@
-import { Component, Input } from '@angular/core';
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { ColorData } from './color-data.interface';
 
 @Component({
   selector: 'ako-color-selector',
   templateUrl: './color-selector.component.html',
   styleUrls: ['./color-selector.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => ColorSelectorComponent),
+      multi: true,
+    },
+  ],
 })
-export class ColorSelectorComponent {
+export class ColorSelectorComponent implements ControlValueAccessor {
   @Input() options: ColorData[] = [];
 
   colorSelected: ColorData | null = null;
+  isDisabled = false;
+  onChangeCb?: (color: ColorData) => void;
+  onTouchedCb?: () => void;
 
   constructor() {}
 
   selectColor(color: ColorData): void {
     this.colorSelected = color;
+
+    this.onChangeCb?.(color);
+  }
+
+  writeValue(color: ColorData): void {
+    this.colorSelected = color;
+  }
+  setDisabledState(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
+  registerOnChange(fn: any): void {
+    this.onChangeCb = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouchedCb = fn;
   }
 }
